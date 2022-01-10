630742c66bbbc2ee3719e183d08f8cbb0824b15f
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-01-10 18:03:49 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -6,7 +6,7 @@ from keras.models import load_model
 
 
 #Loading the Model
-model = tf.load_model('plant_disease.h5')
+model = load_model('plant_disease.h5')
 
 #Name of Classes
 CLASS_NAMES = ['Corn-Common_rust', 'Potato-Early_blight', 'Tomato-Bacterial_spot']
